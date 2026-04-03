f7dd0081b68a21e803e70a2d058daee07e4378f8
NamKhanh2128 <Khanh.NDN235755@sis.hust.edu.vn>
2026-04-03 14:59:53 +0700

    Lab02

---
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,23 @@
+public class DigitalVideoDisc {
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	private String title; 
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	
+}
