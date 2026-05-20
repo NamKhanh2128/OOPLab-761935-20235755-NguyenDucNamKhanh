6b678c8d5ee2683095da40df66119626ba90ba0a
NamKhanh2128 <Khanh.NDN235755@sis.hust.edu.vn>
2026-05-20 17:06:33 +0700

    Lab04

---
+++ Lab04/OtherProjects/Lab03/OtherProjects/src/hust/soict/hedspi/lab01/Intro/HelloWorld.java
@@ -0,0 +1,9 @@
+// Example 2: HelloWorld.java
+
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t world!");
+    } 
+}
